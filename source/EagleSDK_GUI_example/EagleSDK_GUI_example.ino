889238703cdd18a145aa55de5765d49768ca4654
Kyle Hagen <khagen@mtc.irisdynamics.com>
2021-01-04 16:26:39 -0800

    add to GUI demo. Remove unused files

--- source/EagleSDK_GUI_example/EagleSDK_GUI_example.ino
+++ source/EagleSDK_GUI_example/EagleSDK_GUI_example.ino
@@ -28,9 +28,26 @@ ExposedIO
 ExposedOutputBar 
   example_fb_slider ("fb_slider_name", 0, 288, -2);   // arguements are display name, minimum slider value, maximum slider value, initial slider value 
 
+ExposedOutputValue
+  example_fb_value ("fb_value_name", -1);
+
 ExposedButton 
   example_button_1 ("button_1_name", 0),    // the first argument is the name, the second is the toggle state of the button. 0 is untoggled, 1 is toggled
   example_button_2 ("button_2_name", -1);   // -1 for the toggle state creates a button that cannot be toggled 
+
+ExposedStatus   // places text or space on the right side of the screen 
+  example_status ("Example_status_text"),   
+  fb_buffers[10] { 
+    {""},{""},{""},{""},{""},{""},{""},{""},{""},{""}
+  };
+
+ExposedLabel  // places text or space on the left side of the screen 
+  example_label ("Example_label_text"),
+  io_buffers[10] { 
+    {""},{""},{""},{""},{""},{""},{""},{""},{""},{""}
+  };
+
+
 
 
 /**
@@ -50,14 +67,26 @@ void loop() {
 // Example of detecting a new conntection to the IrisControls app
 
   if (!IrisControlsAPI::wasConnected() && IrisControlsAPI::isConnected()) {
-    Printl("New connection detected...");
+    Printl("New connection detected...");   // Print, Printl, Pspace, Ptab functions will all write to the IrisControls console 
     Print("}my_name_is ");   Print(IrisControlsAPI::name);   Print("\r");
 
     // Here is where the initial state of the GUI is set up 
+    for (int i = 0; i<5; i++) { 
+      fb_buffers[i].show();
+      io_buffers[i].show();
+    }
     example_io_slider.show(); 
     example_fb_slider.show(); 
+    example_fb_value.show();
     example_button_1.show(); 
     example_button_2.show(); 
+    example_status.show();
+    example_label.show();
+    
+    for (int i = 5; i<10; i++) { 
+      fb_buffers[i].show();
+      io_buffers[i].show();
+    }
     
   }  
 
@@ -73,13 +102,24 @@ void loop() {
 
     if (example_button_2.pressed()) {
       Printl("Button 2 pressed"); 
+    }
+
+    static int button1_state = 0; 
+    if ( example_button_1.toggled_on() != button1_state ) { 
+      button1_state = example_button_1.toggled_on();
       example_button_1.toggled_on() ? Printl("Button 1 is toggled on"): Printl("Button 1 is toggled off"); 
+      if (button1_state) { 
+        example_status.update("A_<b>bold</b>_status"); 
+      }
+      else { 
+        example_status.update("A_<font_color=#00EE00>colored</font>_status");         
+      }
     }
 
     int slider_value = example_io_slider.update();    // read io slider
     example_fb_slider.update(slider_value); // write fb slider
+    example_fb_value.update(slider_value);
     
-    Pline("}yo:)");        
   }
 }
 
